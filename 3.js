b8d5d8fb2f2ead9803e2be0a5faf6a431bee01a5
Stanislav <9323354@gmail.com>
2023-11-28 20:08:30 +0300

    Third task completed

--- 3.js
+++ 3.js
@@ -23,4 +23,7 @@ const products = [
   },
 ];
 
-// Здесь пишем решение, данный комментарий необходимо стереть.
+products.forEach(element => {
+  element.price -= element.price * (15 / 100);
+});
+console.log(products);
